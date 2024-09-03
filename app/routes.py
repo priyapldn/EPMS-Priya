d08731683e985a8e016a186fd8883427d3efbd74
Priya P <priya.pldn2022@gmail.com>
2024-09-03 11:27:37 +0100

    feat: login and register pages, temporary routes added

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,18 @@ from flask import render_template, Blueprint
 main = Blueprint('main', __name__)
 
 @main.route("/")
+@main.route("/login")
+def login():
+    return render_template('login.html')
+
+@main.route("/register")
+def register():
+    return render_template('register.html')
+
 @main.route("/home")
 def home():
     return render_template('home.html')
+
+@main.route("/create-review")
+def create_review():
+    return render_template('create_review.html')
