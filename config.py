0abcfbbd786fceff8b2b83ecfcb54a1c6b0f6e59
Priya P <priya.pldn2022@gmail.com>
2024-09-10 21:11:05 +0100

    fix: removing sensitive info

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-
 class Config:
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     APP_DIR = os.path.join(BASEDIR, 'app')
@@ -10,8 +9,9 @@ class Config:
     if not os.path.exists(instance_dir):
         os.makedirs(instance_dir)
 
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(APP_DIR, 'instance', 'epmstore.db')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(APP_DIR, 'instance', 'epmstore.db')}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
-    SECRET_KEY = os.getenv('SECRET_KEY', 'a9e3c5383ad2c41293d572afe33d844f1ae4f3248a842524e572abdf62034823')
+    # Use the environment variable for SECRET_KEY, fallback to None if not set
+    SECRET_KEY = os.getenv('SECRET_KEY')
