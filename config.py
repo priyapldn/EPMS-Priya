89b3eab6ee9a0eb0c321a9134240cace737a72e2
priyapldn <119672275+priyapldn@users.noreply.github.com>
2025-02-24 16:36:34 +0000

    dev --> staging (#31) (#32)
    * build: cicd setup
    
    * fix: include secret key var for tests to work
    
    * refactor: with codecov token added to github
    
    * fix: remove secret key
    
    * refactor: small text change
    
    * build: zap scanning
    
    * build: use v4 zap
    
    * build: comment out rules file
    
    * build: test for zap scan
    
    * build: test for zap scan
    
    * build: attempt to fix errors on zap scans
    
    * build: working cicd with zap scan
    
    * build: splitting up cicd into different files
    
    * build: update ci-cd-deploy.yaml
    
    * build: specify branches on cicd deploy
    
    * build: cicd deploy edit

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
 import os
 
+
 class Config:
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    APP_DIR = os.path.join(BASEDIR, 'app')
+    APP_DIR = os.path.join(BASEDIR, "app")
 
     # Create instance folder if it doesn't exist
-    instance_dir = os.path.join(APP_DIR, 'instance')
+    instance_dir = os.path.join(APP_DIR, "instance")
     if not os.path.exists(instance_dir):
         os.makedirs(instance_dir)
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(APP_DIR, 'instance', 'epmstore.db')}")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", f"sqlite:///{os.path.join(APP_DIR, 'instance', 'epmstore.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
     # Use the environment variable for SECRET_KEY, fallback to None if not set
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY"
+    )
